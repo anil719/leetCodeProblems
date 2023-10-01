4d2f49272dc719687a934b8fa7dcdf1f7c8ac435
Anil Terapalli <117853771+anil719@users.noreply.github.com>
2023-10-01 11:25:34 +0530

    Time: 0 ms (100.00%) | Memory: 39.5 MB (91.89%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,10 +1,24 @@
 class Solution {
-    // public int rob(int[] arr) {
-    //     int n = arr.length;
-    //     if(n == 1) return arr[0];
-    //     if(n == 2) return Math.max(arr[0], arr[1]);
-    //     return helper(arr, 0);
-    // }
+    public int rob(int[] arr) {
+        int n = arr.length;
+        if(n == 1) return arr[0];
+        if(n == 2) return Math.max(arr[0], arr[1]);
+
+
+        int prev2 = 0, prev1 = arr[0];
+        for(int i = 1; i < n; i++){
+            int exc = prev1;
+            int incl = prev2 + arr[i];
+            int curr = Math.max(exc, incl);
+            prev2 = prev1;
+            prev1 = curr;
+        } 
+        return prev1;
+
+
+
+       /// return helper(arr, 0);
+    }
     // public int helper(int[] arr, int idx){
     //     int n = arr.length;
     //     if(idx > n-1) return 0;
@@ -15,19 +29,19 @@ class Solution {
     //     return Math.max(inc, exc);
     // }
 
-     public int rob(int[] arr) {
-        int n = arr.length;
-        if(n == 1) return arr[0];
+    //  public int rob(int[] arr) {
+    //     int n = arr.length;
+    //     if(n == 1) return arr[0];
 
-        int[] dp = new int[n];
-        dp[0] = arr[0];
-        dp[1] = Math.max(arr[0], arr[1]);
+    //     int[] dp = new int[n];
+    //     dp[0] = arr[0];
+    //     dp[1] = Math.max(arr[0], arr[1]);
 
-        for(int i = 2; i < n; i++){
-            int inc = dp[i-2] + arr[i];
-            int exc = dp[i-1];
-            dp[i] = Math.max(inc, exc);
-        }
-        return dp[n-1];
-    }
+    //     for(int i = 2; i < n; i++){
+    //         int inc = dp[i-2] + arr[i];
+    //         int exc = dp[i-1];
+    //         dp[i] = Math.max(inc, exc);
+    //     }
+    //     return dp[n-1];
+    // }
 }
