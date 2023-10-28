31d5fd630c94cb688fe5cdb9c4e1817a90484303
Anil Terapalli <117853771+anil719@users.noreply.github.com>
2023-10-28 13:12:40 +0530

    Time: 2 ms (55.22%) | Memory: 43.1 MB (74.04%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -27,31 +27,67 @@ class Solution {
 
         /*       T.C --> O(n^3)     S.C --> O(1)
 
-        int n = arr.length;
-        int maxProduct = Integer.MIN_VALUE;
+                int n = arr.length;
+                int maxProduct = Integer.MIN_VALUE;
 
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                int product = 1;
-                for(int k = i; k <= j; k++) product *= arr[k];
-                maxProduct = Math.max(maxProduct, product);
-            }
-        }
-        return maxProduct;
+                for(int i = 0; i < n; i++){
+                    for(int j = i; j < n; j++){
+                        int product = 1;
+                        for(int k = i; k <= j; k++) product *= arr[k];
+                        maxProduct = Math.max(maxProduct, product);
+                    }
+                }
+                return maxProduct;
 
         */
 
-        int n = arr.length;
-        int maxProduct = Integer.MIN_VALUE;
+        /*      T.C --> O(n^2)     S.C --> O(1)
 
-        for(int i = 0; i < n; i++){
-            int product = 1;
-            for(int j = i; j < n; j++){
-                product *= arr[j];
-                maxProduct = Math.max(maxProduct, product);
+                int n = arr.length;
+                int maxProduct = Integer.MIN_VALUE;
+
+                for(int i = 0; i < n; i++){
+                    int product = 1;
+                    for(int j = i; j < n; j++){
+                        product *= arr[j];
+                        maxProduct = Math.max(maxProduct, product);
+                    }
+                }
+                return maxProduct;
+
+        */
+
+        // int n = arr.length;
+        // int maxProduct = arr[0];
+        // int currProduct = arr[0];
+
+        // for(int i = 1; i < n; i++){
+        //     currProduct = Math.max(currProduct*arr[i], arr[i]);
+        //     maxProduct = Math.max(maxProduct, currProduct);
+        // }
+        // return maxProduct;
+
+
+        int n = arr.length;
+        int maxProduct = arr[0];
+        int minProduct = arr[0];
+        int result = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < 0) {
+                int temp = maxProduct;
+                maxProduct = minProduct;
+                minProduct = temp;
             }
+            
+            maxProduct = Math.max(arr[i], maxProduct * arr[i]);
+            minProduct = Math.min(arr[i], minProduct * arr[i]);
+            
+            result = Math.max(result, maxProduct);
         }
-        return maxProduct;
+
+        return result;
+
 
 
 
