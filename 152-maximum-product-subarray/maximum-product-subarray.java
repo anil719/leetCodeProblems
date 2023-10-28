c87472f1b8bc072f86db79be479e2db0bfcd7289
Anil Terapalli <117853771+anil719@users.noreply.github.com>
2023-10-28 12:59:58 +0530

    Time: 271 ms (5.73%) | Memory: 43.4 MB (35.41%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,5 +1,6 @@
 class Solution {
     public int maxProduct(int[] arr) {
+       /*
         int n = arr.length;
         //prefix Maxproduct
 
@@ -21,5 +22,38 @@ class Solution {
         }
 
         return MaxProduct;
+        
+        */
+
+        /*       T.C --> O(n^3)     S.C --> O(1)
+
+        int n = arr.length;
+        int maxProduct = Integer.MIN_VALUE;
+
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                int product = 1;
+                for(int k = i; k <= j; k++) product *= arr[k];
+                maxProduct = Math.max(maxProduct, product);
+            }
+        }
+        return maxProduct;
+
+        */
+
+        int n = arr.length;
+        int maxProduct = Integer.MIN_VALUE;
+
+        for(int i = 0; i < n; i++){
+            int product = 1;
+            for(int j = i; j < n; j++){
+                product *= arr[j];
+                maxProduct = Math.max(maxProduct, product);
+            }
+        }
+        return maxProduct;
+
+
+
     }
 }
