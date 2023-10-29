381ac857f4be0577d5e5dbe6ae2acc647d1d0ce8
Anil Terapalli <117853771+anil719@users.noreply.github.com>
2023-10-29 12:22:23 +0530

    Time: 9 ms (89.25%) | Memory: 56.2 MB (31.65%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -10,19 +10,45 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        PriorityQueue<Integer> set = new PriorityQueue<>();
-        ListNode curr = head;
-        while(curr != null){
-            set.add(curr.val);
+       if(head == null || head.next == null) return head;
+
+       ListNode head1 = head, head2 = splitAtMid(head);
+
+       head1 = sortList(head1);
+       head2 = sortList(head2);
+
+       ListNode answerNode = new ListNode(-1);
+        ListNode curr = answerNode;
+
+        while(head1 != null && head2 != null){
+            if(head1.val < head2.val){
+                curr.next = head1;
+                head1 = head1.next;
+            }
+            else {
+                curr.next = head2;
+                head2 = head2.next;
+            }
             curr = curr.next;
         }
 
-        ListNode answer = new ListNode(-1);
-        curr = answer ;
-       while(!set.isEmpty()){
-            curr.next = new ListNode(set.poll());
-            curr = curr.next;
+        if(head1 != null) curr.next = head1;
+        if(head2 != null) curr.next = head2;
+
+        return answerNode.next;
+
+
+    }
+    
+    public ListNode splitAtMid(ListNode head){
+
+        ListNode prev = null, slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return answer.next;
+        prev.next = null;
+        return slow;
     }
 }
