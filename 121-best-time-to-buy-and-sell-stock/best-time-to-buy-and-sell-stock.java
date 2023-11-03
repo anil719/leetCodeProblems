81265cb910cc8de652f42dceb6f6b4b1e921a1ea
Anil Terapalli <117853771+anil719@users.noreply.github.com>
2023-11-03 15:40:43 +0530

    Time: 2 ms (75.82%) | Memory: 60.8 MB (69.10%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,15 +1,37 @@
 class Solution {
-    public int maxProfit(int[] arr) {
-        int n = arr.length;
-        if(n == 1) return 0;
+    /*      TC --> O(n^2)  &&  SC--> O(1)
+     public int maxProfit(int[] prices) {
         
-        int minPrice = arr[0];
-        int profit = Integer.MIN_VALUE;
-        for(int i = 1; i < n; i++){
-            minPrice = Math.min(minPrice, arr[i]);
-            int cProfit = arr[i]-minPrice;
-            profit = Math.max(profit, cProfit);
+        int n = prices.length;
+        if(n == 0) return 0;
+        int MaxProfit = Integer.MIN_VALUE;
+      
+        for(int i = 0; i < n; i++){
+            int maxSellingPoint = prices[i];
+            for(int j = i+1; j < n; j++){
+                maxSellingPoint = Math.max(maxSellingPoint, prices[j]);
+            }
+            int profitOfSellingOnIthDay = maxSellingPoint - prices[i];
+            MaxProfit = Math.max(MaxProfit, profitOfSellingOnIthDay );
         }
-        return profit;
+
+        return MaxProfit;
+    }
+    */
+
+    //      TC --> O(n)  &&  SC--> O(1)
+
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        if(n == 0) return 0;
+        int MaxProfit = 0;
+      
+        int minimumPrice = prices[0];
+        for(int i = 1; i < n; i++){
+           minimumPrice = Math.min(minimumPrice, prices[i]);
+           int profitOfSellingToday = prices[i] - minimumPrice;
+           MaxProfit = Math.max(MaxProfit, profitOfSellingToday);
+        }
+        return MaxProfit;
     }
 }
