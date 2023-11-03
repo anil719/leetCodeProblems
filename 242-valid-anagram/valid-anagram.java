edf3ca4bd3a4ba944c07bf45ae3d873a43e0db6a
Anil Terapalli <117853771+anil719@users.noreply.github.com>
2023-11-03 15:21:10 +0530

    Time: 15 ms (27.45%) | Memory: 43.4 MB (58.37%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+       /*
         int[] freq = new int[26];
         if(s.length() != t.length()) return false;
-
 
         for(char ch : s.toCharArray()) freq[ch-'a']++;
 
@@ -10,8 +10,24 @@ class Solution {
 
          for(int i : freq) if(i != 0) return false;
 
-
          return true;
 
+         */
+       
+        if (s.length() != t.length()) {
+            return false;
+        }
+        Map<Character, Integer> countMap = new HashMap<>();
+        
+        for (char c : s.toCharArray()) {
+            countMap.put(c, countMap.getOrDefault(c, 0) + 1);
+        }
+        for (char c : t.toCharArray()) {
+            if (!countMap.containsKey(c) || countMap.get(c) <= 0) {
+                return false;
+            }
+            countMap.put(c, countMap.get(c) - 1);
+        }
+        return true;
     }
 }
